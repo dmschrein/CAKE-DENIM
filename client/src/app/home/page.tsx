dd6821c340d435cb2621f5afe6e18ce401dfe7e4
dmschrein <darrelmschreiner@gmail.com>
2024-09-28 09:24:47 -0700

    updated assets

--- client/src/app/home/page.tsx
+++ client/src/app/home/page.tsx
@@ -16,7 +16,7 @@ export default function Home() {
         {/* <div className="absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#161616_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:4rem_4rem] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_110%)]" /> */}
         <PopupModal />
         <Image
-          src={"/assets/CD-hero.png"}
+          src={"/assets/CD-Website-2.png"}
           width={1920}
           height={1080}
           alt="plur logo"
