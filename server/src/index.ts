59e196d46f596697a907896c2619b8cd4d0e1e9d
Darrel <darrelmschreiner@gmail.com>
2025-03-11 11:47:39 -0700

    added cake domain to allowed origins

--- server/src/index.ts
+++ server/src/index.ts
@@ -39,6 +39,7 @@ import webhookRoutes from "./routes/webhookRoutes";
       "https://cake-denim-git-development-darrel-schreiners-projects.vercel.app",
       "https://main.d3pnxtvhsudm94.amplifyapp.com",
       "https://cakedenim.com",
+      "https://www.cakedenim.com",
     ];
 
     // Security middleware
